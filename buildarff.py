e061b3893af659223744c8ecd3b20086be77c41f
MapleWorld <ou.ye.canada@hotmail.com>
2017-02-07 20:18:56 -0500

    added basic files

---
+++ buildarff.py
@@ -0,0 +1,265 @@
+import os
+import re
+import sys
+import csv
+import string
+import StringIO
+import itertools
+import HTMLParser
+import numpy as np
+
+'''
+Counts:
+| First person pronouns (feat1)
+| Second person pronouns (feat2)
+| Third person pronouns (feat3)
+| Coordinating conjunctions (feat4)
+| Past-tense verbs (feat5)
+| Future-tense verbs (feat6)
+| Commas (feat7)
+| Colons and semi-colons (feat8)
+| Dashes (feat9)
+| Parentheses (feat10)
+| Ellipses (feat11)
+| Common nouns (feat12)
+| Proper nouns (feat13)
+| Adverbs (feat14)
+| wh-words (feat15)
+| Modern slang acronyms (feat16)
+| Words all in upper case (at least 2 letters long) (feat17)
+Average length of sentences (in tokens) (feat18)
+Average length of tokens, excluding punctuation tokens (in characters) (feat19)
+Number of sentences (feat20)
+
+'''
+
+def split_tweet_into_sentences(tweet):
+    tokens = [x.strip().split(' ') for x in tweet]
+    return [y.split('/') for x in tokens for y in x]
+    
+def word_counter(token_split, candidate_words, index):
+    
+    count = 0
+    
+    for x in token_split:
+        if (x[index].lower() in candidate_words):
+            count += 1
+            
+    return count
+    
+def first_person_pronouns(token_split):
+    candidate_words = ['i', 'me', 'my', 'mine', 'we', 'us', 'our', 'ours']
+    return word_counter(token_split, candidate_words, 0)
+
+def second_person_pronouns(token_split):
+    candidate_words = ['you', 'your', 'yours', 'u', 'ur', 'urs']
+    return word_counter(token_split, candidate_words, 0)
+    
+def third_person_pronouns(token_split):
+    candidate_words = ['he', 'him', 'his', 'she', 'her', 'hers', 'it', 'its', 'they', 'them', 'their', 'theirs']
+    return word_counter(token_split, candidate_words, 0)
+
+def coordinating_conjunctions(token_split):
+    candidate_words = ['CC']
+    return word_counter(token_split, candidate_words, 1)
+
+def past_tense_verbs(token_split):
+    candidate_words = ['VBD']
+    return word_counter(token_split, candidate_words, 1)
+
+def future_tense_verbs(token_split):
+    candidate_words = ["'ll", 'will', 'gonna']
+    count = word_counter(token_split, candidate_words, 0)
+    
+    # Count sequences of going+to+VB
+    for i in range(len(token_split) - 2):
+        if (token_split[i][0].lower() == "going"):
+            if (token_split[i + 1][0].lower() == "to"):
+                if (token_split[i + 2][1].lower() == "VB"):
+                    count += 1
+    
+    return count
+
+def commas(token_split):
+    candidate_words = [',']
+    return word_counter(token_split, candidate_words, 1)
+
+def colons(token_split):
+    candidate_words = [':', ';']
+    return word_counter(token_split, candidate_words, 0)
+    
+def dashes(token_split):
+    candidate_words = ['-']
+    return word_counter(token_split, candidate_words, 0)
+
+def parantheses(token_split):
+    candidate_words = ['(', ')']
+    return word_counter(token_split, candidate_words, 0)
+
+def ellipses(token_split):
+    candidate_words = ['...']
+    return word_counter(token_split, candidate_words, 0)
+    
+def common_nouns(token_split):
+    candidate_words = ['NN', 'NNS']
+    return word_counter(token_split, candidate_words, 1)
+
+def proper_nouns(token_split):
+    candidate_words = ['NNP', 'NNPS']
+    return word_counter(token_split, candidate_words, 1)
+
+
+def adverbs(token_split):
+    candidate_words = ['RB', 'RBR', 'RBS']
+    return word_counter(token_split, candidate_words, 1)
+
+
+def wh_words(token_split):
+    candidate_words = ['WDT', 'WP', 'WP$', 'WRB']
+    return word_counter(token_split, candidate_words, 1)
+    
+def slang_acronyms(token_split):
+    candidate_words = ['smh', 'fwb',  'lmfao', 'lmao', 'lms', 'tbh',  'rofl', 'wtf',
+                       'bff', 'wyd',  'lylc',  'brb',  'atm', 'imao', 'sml',  'btw',
+                       'bw',  'imho', 'fyi',   'ppl',  'sob', 'ttyl', 'imo',  'ltr',
+                       'thx', 'kk',   'omg',   'ttys', 'afn', 'bbs',  'cya',  'ez',
+                       'f2f', 'gtr',  'ic',    'jk',   'k',   'ly',   'ya',   'nm',  'np',
+                       'plz', 'ru',   'so',    'tc',   'tmi', 'ym',   'ur',   'u',   'sol']
+    return word_counter(token_split, candidate_words, 0)
+  
+# Count words all in upper case (at least 2 letters long) (feat17)
+def upper_case_words(token_split):
+    
+    count = 0
+    
+    for x in token_split:
+        if (x[0].isupper() and len(x[0]) > 1):
+            count += 1
+            
+    return count
+
+# Average length of sentences (in tokens) (feat18)
+def sentence_length(sentences, token_split):
+    return len(token_split) / float(len(sentences))
+
+# Questionable
+# Average length of tokens, excluding punctuation tokens (in characters) (feat19)
+def token_length(token_split):
+    token_lengths = []
+    candidate_words = ['#', '$', '.', ',', ':', '(', ')', '"', 'POS']
+    
+    for x in token_split:
+         if x[1] not in candidate_words:
+             token_lengths.append(len(x[0]))
+             
+    return sum(token_lengths) / float(len(token_lengths))
+
+# Number of sentences (feat20)
+def number_sentences(sentences):
+    return len(sentences)
+    
+    
+def feature_extraction(tweet, class_label):
+    
+    sentence_list = split_tweet_into_sentences(tweet)
+    
+    feature_list = []
+    feature_list.append(first_person_pronouns(sentence_list))
+    feature_list.append(second_person_pronouns(sentence_list)) 
+    feature_list.append(third_person_pronouns(sentence_list))
+    feature_list.append(coordinating_conjunctions(sentence_list))
+    feature_list.append(past_tense_verbs(sentence_list))
+    feature_list.append(future_tense_verbs(sentence_list))
+    feature_list.append(commas(sentence_list))
+    feature_list.append(colons(sentence_list))
+    feature_list.append(dashes(sentence_list))
+    feature_list.append(parantheses(sentence_list))
+    feature_list.append(ellipses(sentence_list))
+    feature_list.append(common_nouns(sentence_list))
+    feature_list.append(proper_nouns(sentence_list))
+    feature_list.append(adverbs(sentence_list))
+    feature_list.append(wh_words(sentence_list))
+    feature_list.append(slang_acronyms(sentence_list))
+    feature_list.append(upper_case_words(sentence_list))
+    feature_list.append(sentence_length(tweet, sentence_list))
+    feature_list.append(token_length(sentence_list))
+    feature_list.append(number_sentences(tweet))
+    
+    print feature_list
+    
+    output_str = ', '.join(str(x) for x in feature_list) 
+    output_str += ", " + str(class_label) + "\n"
+    
+    print output_str
+    return output_str
+    
+    
+    
+def buildarff(input_file, output_file, max_tweet_per_class):
+    
+    tweet = []
+    output_str = ""
+    class_label = 0
+    class_counters = [-1, 0, 0, 0, 0]
+    
+    if max_tweet_per_class >= 20000:
+        max_tweet_per_class = float("inf")
+        
+    for line in input_file: 
+        if line.startswith('<A='):
+            if tweet != []:
+                output_str = feature_extraction(tweet, class_label)
+                
+            tweet = []
+            class_label = int(line[3])
+            
+            if class_counters[class_label] < max_tweet_per_class:
+                class_counters[class_label] += 1;
+                output_file.write(output_str)
+        else:
+            tweet.append(line)
+            
+    output_str = feature_extraction(tweet, class_label)
+    if class_counters[class_label] < max_tweet_per_class:
+        class_counters[class_label] += 1;
+        output_file.write(output_str)
+    
+    print "Class Counter:" + class_counters
+    
+    
+if __name__ == "__main__":
+    
+    max_tweet_per_class = float("inf")
+    
+    if len(sys.argv) == 3:
+        input_file_name = sys.argv[1]
+        output_file_name = sys.argv[2]
+    elif len(sys.argv) == 4:
+        input_file_name = sys.argv[1]
+        output_file_name = sys.argv[2]  
+        max_tweet_per_class = sys.argv[3]
+    else:
+        print "Invalid number of parameters"
+        sys.exit()
+
+    #input_file_name = "./output/test_output.csv"
+    input_file_name = "./output/train.twt"
+    output_file_name = "./output/train.arff"
+
+    with open(input_file_name, "r") as input:
+        with open(output_file_name, "w") as output:
+            buildarff(input, output, max_tweet_per_class)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ twtt.py
@@ -0,0 +1,222 @@
+# Tweet Tokenize and Tag
+
+import re
+import csv
+import string
+import NLPlib
+import itertools
+from HTMLParser import HTMLParser
+
+# Remove html tags and attributes
+def twtt1(tweet):
+    new_tweet = re.sub(r'<[^>]+>', '', tweet).strip()
+    print "twtt1: " + new_tweet
+    return new_tweet
+
+# Replace html name with ASCII    
+def twtt2(tweet):
+    
+    new_tweet = ""
+    parser = HTMLParser()
+    for char in tweet:
+        if (char in string.printable):
+            new_tweet += char
+            
+    new_tweet = parser.unescape(new_tweet)
+    print "twtt2: " + new_tweet
+    return new_tweet
+  
+# Remove URL
+def twtt3(tweet):
+    case_insensitive = re.compile(r'(http:|www.)\S+', re.IGNORECASE)
+    new_tweet = case_insensitive.sub('', tweet).strip()
+    print "twtt3: " + new_tweet
+    return new_tweet
+    
+# Remove the first Twitter username (@) and hash tags
+def twtt4(tweet):
+    new_tweet = re.sub(r'@\S+', '', tweet, 1).strip()
+    new_tweet = re.sub(r'#\S+', '', new_tweet).strip()
+    print "twtt4: " + new_tweet
+    return new_tweet
+
+def create_abbrev_set():
+    abbrev_set = set()
+    
+    with open('./language/abbrev.english', 'rb') as abbrevs:
+        for line in abbrevs:
+            abbrev_set.add(line.strip())
+            
+    return abbrev_set
+    
+# split by sentence
+def twtt5(tweet):
+    '''
+        # 1. Anything ending in .?! declared a sentence
+        # 2. Sentence boundary moved after quotation mark, if any ex. He said, "I am coming."
+        # 3. Period boundary is disqualified if it preceded by an element in abbrev_set
+        #    <We could look for capitals after an EOS, but nobody uses capitals on twitter>
+        #    <Both sides of :;- could also be thought of as sentence>
+    '''
+    
+    tweet = re.sub(r' +', ' ', tweet).strip()
+    
+    if 0 == len(tweet): 
+        return [tweet]
+    
+    abbrev_set = create_abbrev_set()
+    split_by_space = tweet.split(' ')
+    
+    new_line = ""
+    sentences = []
+    
+    for word in split_by_space:
+        new_line += " " + word
+        for puncution in {"?", "!", "."}:
+            if (puncution in word):
+                for abbrev in abbrev_set:
+                    if (abbrev not in word):
+                        sentences.append(new_line)
+                        new_line = ""
+                        break
+    
+    
+    if (new_line != ""):
+        sentences.append(new_line)
+    
+    if (len(sentences) == 0):
+        sentences = [tweet]
+    
+    print "twtt5:" + ', '.join(sentences) 
+    return sentences
+ 
+def twtt7(tweet):
+    # 1. Split on all punctuation symbols, where a given symbol is repeated once or more
+    sentence_1 = re.sub(r"((["+ string.punctuation + "])\\2*)", r" \1 ", tweet).strip()
+    
+    # 2. Remove double space
+    sentence_2 = ' '.join(sentence_1.split('  '))
+    
+    # 3. Join clitics and contractions where ' occurs mid-word
+    sentence_3 = re.sub(r"(') ([A-Za-z] )", r"\1\2", sentence_2)
+    
+    # 4. Join e.g.
+    sentence_4 = re.sub(r" e . g . ", r" e.g. ", sentence_3)
+    
+    print "twtt7: " + sentence_4
+    return sentence_4
+
+# Tag Sentence
+# Each token is tagged with its part-of-speech.
+# A tagged token consists of a word, the `/' symbol, and the tag (e.g., dog/NN)
+def twtt8(tweet, tagger):
+    word_list = tweet.split(" ")
+    tag_list = tagger.tag(word_list)
+    result = ' '.join([x[0] + "/" + x[1] for x in zip(word_list, tag_list)])
+    print "twtt8: " + result
+    return result
+
+# Before each tweet is demarcation A=# in <> which occurs on its own line
+# where # is the numeric class of the tweet (0, or 4).
+# The polarity of the tweet (0 = negative emotion, 4 = positive emotion)
+def twtt9(tweet, class_type):
+    prepend = "<A={}>".format(class_type)
+    new_line = [prepend] + tweet
+    print new_line
+    return new_line
+
+##### Take in an input file name, and an output file, and a student ID.
+# Negative group IDs use all samples available
+def twtt(output_file_name, input_file_name, SID):
+    
+    X = SID % 80
+    line_index_list = []
+    row_count = sum(1 for row in csv.reader(open(input_file_name)))
+    
+    tagger = NLPlib.NLPlib()
+    if (row_count >= 1600000):
+        line_index_list.append([X * 10000, (X + 1) * 10000])
+        line_index_list.append([800000 + X * 10000, 800000 + (X + 1) * 10000])
+    else:   
+        line_index_list.append([0, 20000])
+        
+
+    with open(output_file_name, "w") as output_file:
+        with open(input_file_name, 'rb') as csvfile:
+            
+            spamreader = csv.reader(csvfile)
+            
+            for line_index in line_index_list:
+                for row in itertools.islice(spamreader, line_index[0], line_index[1]):
+                    tweet = row[5]
+                    class_type = int(row[0])
+                    new_line = twtt1(tweet)
+                    new_line = twtt2(new_line)
+                    new_line = twtt3(new_line)
+                    new_line = twtt4(new_line)
+                    sentences = twtt5(new_line)
+                    
+                    if len(sentences) > 0:
+                        sentences = [twtt7(sentence) for sentence in sentences]
+                        sentences = [twtt8(sentence, tagger) for sentence in sentences]
+                    
+                    sentences = twtt9(sentences, class_type)
+                    
+                    for sentence in sentences:
+                        output_file.write(sentence + '\n')
+                        
+                    
+if __name__ == "__main__":
+
+    #twtt('testdata_output.csv', 'testdata.manual.2009.06.14.csv', 999768327)
+    #twtt('training_output.csv', 'training.1600000.processed.noemoticon.csv', 999768327)
+    twtt('./output/test_output.csv', 'test.csv', 999768327)
+    
+
+    '''
+    input0 = 'Argh� Fantasy Surfer Carnage� Dane, Marlon &amp; Nat C all out!  #fantasysurfer converted HTML To ASCII: &'
+    input1 = '@switchfoot http://twitpic.com/2y1zl - <t>asda</t> - @gg www.omg.com &apos; Awwwf Third Day to!!! +- do &gt; it. ;D #omg'
+    input2 = "This tweet's the best tweet!! Everybody says so."
+    
+    new_line = twtt1(input0)
+    new_line = twtt2(new_line)
+    new_line = twtt3(new_line)
+    new_line = twtt4(new_line)
+    
+    sentences = twtt5(new_line)
+    
+    if len(sentences) > 0:
+        sentences = [twtt7(sentence) for sentence in sentences]
+        sentences = [twtt8(sentence) for sentence in sentences]
+    
+    class_type = 0
+    sentences = twtt9(sentences, class_type)
+    
+    '''
+    
+    
+    '''
+    sentence_1 = re.sub(r"((["+ string.punctuation + "])\\2*)", r" \1 ", input).strip()
+    print sentence_1
+    sentence_1 = ' '.join(sentence_1.split('  '))
+    print sentence_1
+    '''
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
